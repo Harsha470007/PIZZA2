0ed4ef08e226d83ddcfb1ecd5918daa634690dac
Harsha470007 <theharshaguy470007@gmail.com>
2020-10-04 07:26:50 +0530

    slight correction

--- tutorial.py
+++ tutorial.py
@@ -40,7 +40,7 @@ if ans.lower() not in ["yes", "y"]:
 print("\nMENU\n")
 
 menu = my_local_dominos.get_menu()
-order = Order.begin_customer_order(customer, my_local_dominos, "in")
+order = Order.begin_customer_order(customer, my_local_dominos)
 
 while True:
 	searchMenu(menu)
